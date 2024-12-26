820fbc6ca6f6ba4ef885dd3489623f2179b9bbb6
awsdaws <152947131+awsdaws@users.noreply.github.com>
2024-12-26 10:43:18 +0800

    Add files via upload

---
+++ 20241226001CS/Car.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20241226001CS
+{
+    internal class Car
+    {
+        //屬性
+        public int Id { get; set; }  //車子的ID,車牌
+        public string Owner { get; set; }  //車子主人的名字
+        public string Description { get; set; } //關於這台車描述
+        public double Price {  get; set; }  
+        public string Brand { get; set; } //品牌
+        public string Model { get; set; }  //型號
+
+        private double _tax; //關稅
+        public double Tax
+        {
+            get { return this._tax; }
+            set { this._tax = value; }
+        }
+
+        //建構子函式1
+        public Car()     {       }
+
+        //建構子函式2 
+        public Car(int id, string vOwner, string description, double price, string brand, string model, double tax)
+        {
+            Id = id;
+            Owner = vOwner;
+            Description = description;
+            Price = price;
+            Brand = brand;
+            Model = model;
+            Tax = tax;
+           
+        }
+
+        public void PrintAllAttrib()
+        {
+            Console.WriteLine("ID is:" + this.Id.ToString());
+            Console.WriteLine("Owener is :"+this.Owner.ToString());
+            //類推..把所有屬性印出來.
+
+        }
+    }
+}
---
+++ 20241226001CS/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace _20241226001CS
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Puppy DennisPuppy = new Puppy();
+            //設定DennisCar的屬性值.
+            DennisPuppy.Id = 1;
+            DennisPuppy.Name = "Dennis";
+            DennisPuppy.Variety ="abc";
+            DennisPuppy.Weight =150;
+            DennisPuppy.Personality ="happy";
+            DennisPuppy.Color ="green";
+            //類堆..所有屬性都要設.
+
+            //最後,叫用DennisCar的方法PrintAllAttrib( )印出所有屬性參數.
+            DennisPuppy.CallMyPuppyName();
+            Console.WriteLine();
+            //===============================================
+            //第二個 使用有參數的建構子設一台車MarysCar.
+            //經由建構子函式2 來設定所有屬性
+            Puppy MarysPuppy = new Puppy(2, "Mary","def",30,"unhappy","black");
+
+            //最後,叫用 MarysCar的方法PrintAllAttrib( )印出所有屬性參數.
+            MarysPuppy.CallMyPuppyName();
+
+
+        }
+    }
+}
